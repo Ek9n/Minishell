dda4cc00cbbc0d592fa5d8081f042106474b47e8
humbug <humbug>
2023-11-08 12:14:20 +0100

    update

--- srcs/Main.c
+++ srcs/Main.c
@@ -6,7 +6,7 @@
 /*   By: hstein <hstein@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 11:40:45 by jfoltan           #+#    #+#             */
-/*   Updated: 2023/11/07 23:40:51 by hstein           ###   ########.fr       */
+/*   Updated: 2023/11/08 12:14:04 by hstein           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,45 +24,15 @@ void	signal_handler(int sig, siginfo_t *info, void *context)
 	fflush(0);
 }
 
-// int	main(int argc, char **argv, char **envp)
-// {
-// 	struct sigaction	act;
-// 	t_words				**words;
-// 	char				*input;
-// 	int					b;
-
-// 	(void)argc;
-// 	(void)argv; // We should duplicate argc argv and envp to our struct,
-// 	b = 0;
-// 	words = NULL;
-// 	sigemptyset(&act.sa_mask);
-// 	act.sa_sigaction = &signal_handler;
-// 	act.sa_flags = SA_SIGINFO;
-// 	sigaction(SIGQUIT, &act, NULL);
-// 	sigaction(SIGINT, &act, NULL);
-// 	while (true)
-// 	{
-// 		input = readline("Minishell>>: ");
-// 		if (input)
-// 			add_history(input); // history works
-// 		words = init_word_stack(input, words,envp);
-// 		parser(words); // maybe just having one copy and passing that to functions as arguments is better, idk. 
-// 	}
-// 	while (words[b] != NULL)
-// 	{
-// 		printf("word: %s at index: %d\n", words[b]->word, b);
-// 		printf("Token: %s at index: %d\n", words[b]->token_after_word, b);
-// 		b++;
-// 	}
-// }
-
 int	main(int argc, char **argv, char **envp)
 {
 	struct sigaction	act;
-	static t_words **words;
-	char * input;
-	int		b;
+	t_words				**words;
+	char				*input;
+	int					b;
 
+	(void)argc;
+	(void)argv; // We should duplicate argc argv and envp to our struct,
 	b = 0;
 	words = NULL;
 	sigemptyset(&act.sa_mask);
@@ -70,17 +40,47 @@ int	main(int argc, char **argv, char **envp)
 	act.sa_flags = SA_SIGINFO;
 	sigaction(SIGQUIT, &act, NULL);
 	sigaction(SIGINT, &act, NULL);
-	words = malloc(sizeof(t_words*));
-	words[0] = malloc(sizeof(t_words*));
-	words[0]->num_of_elements = 1;
-	while(true)
+	while (true)
 	{
 		input = readline("Minishell>>: ");
 		if (input)
 			add_history(input); // history works
-		words[0]->word = malloc(1000);
-		words[0]->word = input;
-		parser(words);
-		free(words[0]->word);
+		words = init_word_stack(input, words,envp);
+		parser(words); // maybe just having one copy and passing that to functions as arguments is better, idk. 
+	}
+	while (words[b] != NULL)
+	{
+		printf("word: %s at index: %d\n", words[b]->word, b);
+		printf("Token: %s at index: %d\n", words[b]->token_after_word, b);
+		b++;
 	}
 }
+
+// int	main(int argc, char **argv, char **envp)
+// {
+// 	struct sigaction	act;
+// 	static t_words **words;
+// 	char * input;
+// 	int		b;
+
+// 	b = 0;
+// 	words = NULL;
+// 	sigemptyset(&act.sa_mask);
+// 	act.sa_sigaction = &signal_handler;
+// 	act.sa_flags = SA_SIGINFO;
+// 	sigaction(SIGQUIT, &act, NULL);
+// 	sigaction(SIGINT, &act, NULL);
+// 	words = malloc(sizeof(t_words*));
+// 	words[0] = malloc(sizeof(t_words*));
+// 	words[0]->num_of_elements = 1;
+// 	while(true)
+// 	{
+// 		input = readline("Minishell>>: ");
+// 		if (input)
+// 			add_history(input); // history works
+// 		words[0]->word = malloc(1000);
+// 		words[0]->word = input;
+// 		parser(words);
+// 		free(words[0]->word);
+// 	}
+// }
