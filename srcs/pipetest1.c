5cb299562de6e38172ff390ce62bf1c4c2283700
humbug <humbug>
2023-11-09 15:11:51 +0100

    push

--- srcs/pipetest1.c
+++ srcs/pipetest1.c
@@ -5,6 +5,24 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+// void	executor(char *clean_word, char **envp)
+// {
+// 	int pid;
+// 	(void)envp;
+// 	char	**args = ft_split(clean_word, ' ');
+
+// 	printf("->%s\n", clean_word);
+// 	if ((pid = fork()) == -1)
+// 	perror("fork error");
+// 	else if (pid == 0) 
+// 	{
+// 		execve(ft_strjoin("/bin/",args[0]), args, NULL);
+// 		printf("Return not expected. Must be an execve error.\n");
+// 	}
+// 	else
+// 		waitpid(0, NULL, 0);
+// }
+
 // int	main(int argc, char**argv)
 int	main()
 {
@@ -27,8 +45,16 @@ int	main()
 
 	if (pid == 0)
 	{
-
+		// open(pipe_fd[1]);
+		dup2(pipe_fd[1], STDOUT_FILENO);
+		printf("ChildSTDOUT_FILENO:%d\n", STDOUT_FILENO);
+		close(pipe_fd[1]);
+		
 	}
+	else
+		printf("ParentSTDOUT_FILENO:%d\n", STDOUT_FILENO);
+
+	waitpid(pid, NULL, 0);
 
 	return (0);
 }
