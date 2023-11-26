0f52719d96fa3f3fdd5f7efb89430f51ceeca408
FlyingLamb <h.humbug@outlook.com>
2023-11-26 17:49:11 +0100

    push

--- srcs/Parser.c
+++ srcs/Parser.c
@@ -211,6 +211,102 @@ void	printpipe(int read_fd)
 // 	return (i);
 // }
 
+int	cnt_pipes(t_words **INstruct)
+{
+	int	 i;
+
+	i = 0;
+	while (INstruct[i+1] != NULL && INstruct[i]->token_after_word != NULL && INstruct[i]->token_after_word[0] == '|')
+		i++;
+	return (i);
+}
+
+
+int piperino7(t_words **INstruct)
+{
+	char	**cmd1;
+	char	*path1;
+	int		**pipe_fd;
+	int		pids[100];
+	pid_t	pid;
+
+	pipe_fd = malloc(200 * sizeof(int*));
+	for (int j = 0; j < 3; j++)
+	{
+		pipe_fd[j] = malloc(2 * sizeof(int));
+		if (pipe(pipe_fd[j]) == -1)
+			error_exit("(piperino5) Pipe creation failed\n");
+		printf("pipe[%d]:read=%d, write=%d\n", j, pipe_fd[j][0], pipe_fd[j][1]);
+	}
+
+	cmd1 = ft_split(INstruct[0]->word_clean, ' ');
+	path1 = ft_strjoin("/bin/", cmd1[0]);
+	pid = fork();
+	if (pid == 0)
+	{
+		printf("Start:\n");
+		dup2(pipe_fd[0][1], STDOUT_FILENO);
+		close(pipe_fd[0][0]);
+		close(pipe_fd[0][1]);
+
+		execve(path1, cmd1, NULL);
+		perror("(piperino5) Exec1 failed");
+	}
+	// free_piperino2(INstruct[i], cmd1, path1);
+	cmd1 = ft_split(INstruct[1]->word_clean, ' ');
+	path1 = ft_strjoin("/bin/", cmd1[0]);
+	pid = fork();
+	if (pid == 0)
+	{
+		printf("Mid:\n");
+		dup2(pipe_fd[0][0], STDIN_FILENO);
+		close(pipe_fd[0][0]);
+		close(pipe_fd[0][1]);
+		dup2(pipe_fd[1][1], STDOUT_FILENO);
+		close(pipe_fd[1][0]);
+		close(pipe_fd[1][1]);
+
+		execve(path1, cmd1, NULL);
+		perror("(piperino5) Exec1 failed");
+	}
+	// free_piperino2(INstruct[i], cmd1, path1);
+	cmd1 = ft_split(INstruct[2]->word_clean, ' ');
+	path1 = ft_strjoin("/bin/", cmd1[0]);
+	pid = fork();
+	if (pid == 0)
+	{
+		printf("End:\n");
+		dup2(pipe_fd[1][0], STDIN_FILENO);
+		close(pipe_fd[1][0]);
+		close(pipe_fd[1][1]);
+
+		execve(path1, cmd1, NULL);
+		perror("(piperino5) Exec1 failed");
+	}
+	// free_piperino2(INstruct[i], cmd1, path1);
+	int i = 0;
+	while (INstruct[i+1] != NULL && INstruct[i]->token_after_word != NULL && INstruct[i]->token_after_word[0] == '|')
+	{
+		close(pipe_fd[i][0]);
+		close(pipe_fd[i][1]);
+		printf("closeread=%d\n", pipe_fd[i][0]);
+		printf("closewrite=%d\n", pipe_fd[i][1]);
+		i++;
+		// free(pipe_fd[j]);
+	}
+	// close(pipe_fd[i][0]);
+	// close(pipe_fd[i][1]);
+	// printf("closeread=%d\n", pipe_fd[i][0]);
+	// printf("closewrite=%d\n", pipe_fd[i][1]);
+	// free(pipe_fd);
+	// kill(pid_to_kill, SIGTERM);
+	for (int i = 0; i < 3; i++)
+		waitpid(-1, NULL, 0);
+	// printf("shieet\n");
+	return (i);
+}
+
+
 int piperino6(t_words **INstruct)
 {
 	char	**cmd1;
@@ -238,30 +334,35 @@ int piperino6(t_words **INstruct)
 		{
 			if (i == 0)
 			{
-				close(pipe_fd[i][0]);
+				printf("Start:\n");
 				dup2(pipe_fd[i][1], STDOUT_FILENO);
+				close(pipe_fd[i][0]);
 				close(pipe_fd[i][1]);
 			}
-			else if (INstruct[i]->token_after_word != NULL)
+			else if (INstruct[i]->token_after_word != NULL && INstruct[i]->token_after_word[0] == '|')
 			{
-				close(pipe_fd[i][0]);
+				printf("Mid:\n");
 				dup2(pipe_fd[i-1][0], STDIN_FILENO);
 				close(pipe_fd[i-1][0]);
+				close(pipe_fd[i-1][1]);
 				dup2(pipe_fd[i][1], STDOUT_FILENO);
+				close(pipe_fd[i][0]);
 				close(pipe_fd[i][1]);
 			}
 			else
 			{
+				printf("End:\n");
 				dup2(pipe_fd[i-1][0], STDIN_FILENO);
 				close(pipe_fd[i-1][0]);
+				close(pipe_fd[i-1][1]);
 			}
 			execve(path1, cmd1, NULL);
 			perror("(piperino5) Exec1 failed");
 		}
 		// printf("hi[%d\n", i);
 
-		if (INstruct[i+1] != NULL) // wenn man das nicht macht, kann man am ende mit str+d nicht beenden..
-			close(pipe_fd[i][1]);
+		// if (INstruct[i+1] != NULL) // wenn man das nicht macht, kann man am ende mit str+d nicht beenden..
+			// close(pipe_fd[i][1]);
 		
 		// if (i != 0)
 		// 	close(pipe_fd[i-1][0]);
@@ -272,9 +373,10 @@ int piperino6(t_words **INstruct)
 	while (INstruct[i+1] != NULL && INstruct[i]->token_after_word != NULL && INstruct[i]->token_after_word[0] == '|')
 	{
 		close(pipe_fd[i][0]);
+		close(pipe_fd[i][1]);
 		printf("closeread=%d\n", pipe_fd[i][0]);
+		printf("closewrite=%d\n", pipe_fd[i][1]);
 		i++;
-		// close(pipe_fd[j][1]);
 		// free(pipe_fd[j]);
 	}
 	close(pipe_fd[i][0]);
@@ -289,58 +391,58 @@ int piperino6(t_words **INstruct)
 	return (i);
 }
 
-// int piperino5(t_words **INstruct)
-// {
-// 	char	**cmd1 = ft_split(INstruct[0]->word_clean, ' ');
-// 	char	*path1 = ft_strjoin("/bin/", cmd1[0]);
-// 	int		pipe_fd[2];
-// 	int		tmp_read_fd;
-// 	pid_t	pid;
+int piperino5(t_words **INstruct)
+{
+	char	**cmd1 = ft_split(INstruct[0]->word_clean, ' ');
+	char	*path1 = ft_strjoin("/bin/", cmd1[0]);
+	int		pipe_fd[2];
+	int		tmp_read_fd;
+	pid_t	pid;
 
-// 	int	i = 0;
-// 	while (INstruct[i+1] != NULL && INstruct[i]->token_after_word != NULL && INstruct[i]->token_after_word[0] == '|')
-// 	{
-// 		if (pipe(pipe_fd) == -1)
-// 			error_exit("(piperino5) Pipe creation failed\n");
-// 		pid = fork();
-// 		if (pid == 0)
-// 		{
-// 			close(pipe_fd[0]);
-// 			if (i != 0)
-// 			{
-// 				dup2(tmp_read_fd, STDIN_FILENO);
-// 				close(tmp_read_fd);
-// 			}
-// 			dup2(pipe_fd[1], STDOUT_FILENO);
-// 			close(pipe_fd[1]);
-// 			execve(path1, cmd1, NULL);
-// 			perror("(piperino5) Exec1 failed");
-// 		}
-// 		waitpid(pid, NULL, 0);
-// 		close(pipe_fd[1]);
-// 		free_piperino(cmd1, path1);
-// 		i++;
-// 		cmd1 = ft_split(INstruct[i]->word_clean, ' ');
-// 		path1 = ft_strjoin("/bin/", cmd1[0]);
-// 		tmp_read_fd = pipe_fd[0];
-// 	}
-// 	// for (int i; i < 2; i++)
-// 	// 	waitpid(-1, NULL, 0);
-// 	pid = fork();
-// 	if (pid == 0)
-// 	{
-// 		dup2(tmp_read_fd, STDIN_FILENO);
-// 		close(tmp_read_fd);
-// 		execve(path1, cmd1, NULL);
-// 		perror("(piperino5) Exec1 failed");
-// 	}
-// 	close(tmp_read_fd);
-// 	close(pipe_fd[0]);
-// 	close(pipe_fd[1]);
-// 	waitpid(-1, NULL, 0);
-// 	free_piperino(cmd1, path1);
-// 	return (i);
-// }
+	int	i = 0;
+	while (INstruct[i+1] != NULL && INstruct[i]->token_after_word != NULL && INstruct[i]->token_after_word[0] == '|')
+	{
+		if (pipe(pipe_fd) == -1)
+			error_exit("(piperino5) Pipe creation failed\n");
+		pid = fork();
+		if (pid == 0)
+		{
+			close(pipe_fd[0]);
+			if (i != 0)
+			{
+				dup2(tmp_read_fd, STDIN_FILENO);
+				close(tmp_read_fd);
+			}
+			dup2(pipe_fd[1], STDOUT_FILENO);
+			close(pipe_fd[1]);
+			execve(path1, cmd1, NULL);
+			perror("(piperino5) Exec1 failed");
+		}
+		waitpid(pid, NULL, 0);
+		close(pipe_fd[1]);
+		free_piperino(cmd1, path1);
+		i++;
+		cmd1 = ft_split(INstruct[i]->word_clean, ' ');
+		path1 = ft_strjoin("/bin/", cmd1[0]);
+		tmp_read_fd = pipe_fd[0];
+	}
+	// for (int i; i < 2; i++)
+	// 	waitpid(-1, NULL, 0);
+	pid = fork();
+	if (pid == 0)
+	{
+		dup2(tmp_read_fd, STDIN_FILENO);
+		close(tmp_read_fd);
+		execve(path1, cmd1, NULL);
+		perror("(piperino5) Exec1 failed");
+	}
+	close(tmp_read_fd);
+	close(pipe_fd[0]);
+	close(pipe_fd[1]);
+	waitpid(pid, NULL, 0);
+	free_piperino(cmd1, path1);
+	return (i);
+}
 
 void	routine(t_words **INstruct)
 {
@@ -352,7 +454,7 @@ void	routine(t_words **INstruct)
 	{
 		if (INstruct[i]->token_after_word != NULL && \
 				INstruct[i]->token_after_word[0] == '|')
-			i += piperino6(&INstruct[i]);
+			i += piperino5(&INstruct[i]);
 		else
 			parser(INstruct[i]);
 		i++;
