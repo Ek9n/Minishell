f5a95be73c1634e21b83b4a0fb3603d3bf088119
Hannes Stein <hstein@c4c5c2.42berlin.de>
2023-12-04 19:20:51 +0100

    push

--- srcs/enviromentvars.c
+++ srcs/enviromentvars.c
@@ -1,4 +1,21 @@
-#include "minishell.h"
+// #include "minishell.h"
+#include "../libft/libft.h"
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	size_t	i;
+	size_t	n;
+	
+	n = ft_strlen(s1);
+	i = 0;
+	while ((s1[i] != '\0' || s2[i] != '\0') && i < n)
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
+	}
+	return (0);
+}
 
 char	**arrdup(char **enviroment)
 {
@@ -88,45 +105,85 @@ char	**delete_env_var(char *name, char **env)
 	return (new_env);
 }
 
-char	**add_env_var(char *name, char **env)
+// char	**add_env_var(char *name, char **env)
+// {
+// 	int		size = cntenv(env);
+// 	int		i;
+// 	char	**new_env;
+
+// 	new_env = ft_calloc(size + 1, sizeof(char *));
+// 	i = 0;
+// 	while (env[i] != NULL)
+// 	{
+// 		new_env[i] = env[i];
+// 		i++;
+// 	}
+// 	new_env[i] = name;
+// 	free(env);
+// 	return (new_env);
+// }
+void	add_env_var(char *name, char ***env)
 {
-	int		size = cntenv(env);
+	int		size = cntenv(*env);
 	int		i;
 	char	**new_env;
 
 	new_env = ft_calloc(size + 1, sizeof(char *));
 	i = 0;
-	while (env[i] != NULL)
+	while (env[0][i] != NULL)
 	{
-		new_env[i] = env[i];
+		new_env[i] = env[0][i];
 		i++;
 	}
 	new_env[i] = name;
-	free(env);
-	return (new_env);
+	free(*env);
+	*env = new_env;
+}
+	// else if (cmp_keyword("export", INstruct->word_clean))
+	// {
+	// 	export(INstruct->word_clean);
+	// }
+
+void	export(char *str, char ***env)
+{
+	// char	**env_new;
+	char	**cmds;
+	int	i;
+
+	cmds = ft_split(str, ' ');
+	i = 0;
+	if (cmds[i + 1] == NULL)
+		printf("print envs here in exportstyle!\n");
+	while (cmds[i])
+	{
+		
+		add_env_var(cmds[i], env);
+		i++;
+	}
+
+
 }
 
-// int	main(int argc, char **argv, char **env)
-// {
-// 	char	**env_lst = arrdup(env);
-// 	// ft_putstr_fd(env_lst[0], 1);
-// 	printf("CNT:%d\n", cntenv(env_lst));
-// 	// printenv(env_lst);
-// 	// env_lst = delete_env_var("OLDPWD", env_lst);
-// 	// char str[] = "HELLOWORLD";
-// 	char *str;
-// 	str = malloc(5);
-// 	str[0] = 'H';
-// 	str[1] = '=';
-// 	str[2] = 'Y';
-// 	str[3] = '\0';
+int	main(int argc, char **argv, char **env)
+{
+	char	**env_lst = arrdup(env);
+	// ft_putstr_fd(env_lst[0], 1);
+	printf("CNT:%d\n", cntenv(env_lst));
+	// printenv(env_lst);
+	// env_lst = delete_env_var("OLDPWD", env_lst);
+	// char str[] = "HELLOWORLD";
+	char *str;
+	str = malloc(50);
+	str = ft_strdup("H=55");
 
-// 	env_lst = add_env_var(str, env_lst);
-// 	// delete_env_var("OLDPWD", &env_lst);
-// 	printf("CNT:%d\n", cntenv(env_lst));
+	// env_lst = add_env_var(str, env_lst);
+	add_env_var(str, &env_lst);
+	// export(str, &env_lst);
+	// delete_env_var("OLDPWD", &env_lst);
+	printf("CNT:%d\n", cntenv(env_lst));
 
-// 	printenv(env_lst);
+	printenv(env_lst);
 
-// 	freeenv(env_lst);
-// 	return (0);
-// }
+	freeenv(env_lst);
+	return (0);
+}
